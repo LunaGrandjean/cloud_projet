f0f7198e75052fe9c1158223a1179f21302fbaec
thibautRV <thibaut.hervet085@gmail.com>
2024-04-18 23:01:08 +0200

    Update dashboard.py

--- dashboard/src/dashboard.py
+++ dashboard/src/dashboard.py
@@ -1,32 +1,55 @@
 import streamlit as st
+import json
 import pandas as pd
 import numpy as np
 import plotly.express as px
 from sqlalchemy import create_engine
+from pandas.io.json import json_normalize
 
 engine = create_engine('postgresql://admin:securepass@db:5432/sensordata')
 
+
+
 def fetch_sensor_data():
     query = """
-    SELECT timestamp, temperature, humidity
+    SELECT timestamp, measures
     FROM sensor_data
     ORDER BY timestamp DESC
     LIMIT 100
     """
     data = pd.read_sql(query, engine)
-    return data
+    
+    # Convertir la chaîne JSON en un dictionnaire Python, supposer que les mesures sont stockées comme une chaîne JSON valide
+    data['measures'] = data['measures'].apply(json.loads)
+
+    # Normaliser les données JSON imbriquées dans une structure de dataframe plat
+    measures_df = json_normalize(data['measures'])
+
+    # Joindre les données normalisées avec le timestamp
+    full_data = data.join(measures_df)
+    
+    return full_data
+
 
 def main():
     st.title('Operator Dashboard')
 
     if st.button('Refresh Data'):
-        data = fetch_sensor_data()
-        st.write(data)
-
-        fig_temp = px.line(data, x='timestamp', y='temperature', labels={'timestamp': 'Timestamp', 'temperature': 'Temperature (°C)'}, title='Temperature Over Time')
+        full_data = fetch_sensor_data()
+        
+        # Affichage du DataFrame
+        st.write(full_data)
+        
+        # Graphique de température
+        fig_temp = px.line(full_data, x='timestamp', y='temperature', 
+                           labels={'temperature': 'Temperature (°C)'}, 
+                           title='Temperature Over Time')
         st.plotly_chart(fig_temp)
-
-        fig_humidity = px.line(data, x='timestamp', y='humidity', labels={'timestamp': 'Timestamp', 'humidity': 'Humidity (%)'}, title='Humidity Over Time')
+        
+        # Graphique d'humidité
+        fig_humidity = px.line(full_data, x='timestamp', y='humidity', 
+                               labels={'humidity': 'Humidity (%)'}, 
+                               title='Humidity Over Time')
         st.plotly_chart(fig_humidity)
 
 if __name__ == "__main__":
